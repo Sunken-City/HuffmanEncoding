a923be79cb8428aa3e58259777177e25564fec4e
picoriley <acloudy@mail.smu.edu>
2014-12-28 19:38:10 -0600

    Practicing with file IO in C++. It's been a while.

--- HuffmanEncoding.cpp
+++ HuffmanEncoding.cpp
@@ -4,14 +4,19 @@
 #include "stdafx.h"
 #include <stdlib.h>
 #include <iostream>
+#include <cstdio>
+#include <string>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int i;
-	cin >> i;
-	cout << i << endl;
+	string testString;
+	cin >> testString;
+	FILE* file;
+	fopen_s(&file, "save.bin", "wb");
+	fwrite(testString.c_str(), sizeof(testString), 1, file);
+	cout << testString << " was written to file." << endl;
 	return 0;
 }
 
